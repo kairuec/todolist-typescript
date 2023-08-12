77b074b48a6536281c7dfb7fe5529857eb167c4a
surface <sakurai1991@gmail.com>
2023-08-12 11:47:29 +0900

    input

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,37 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
+import { log } from 'console';
 
 function App() {
+
+  const [inputValue, setInputValue] = useState("");
+  const [todos, setTodos] = useState<Todo[]>([]);
+
+  type Todo = {
+    inputValue: string;
+    id: number;
+    checked: boolean;
+  }
+
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    console.log(e.target.value);
+
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div>
+        <h2>Todoリスト with Typescript</h2>
+        <form onSubmit={() => { }}>
+          <input
+            type="test"
+            onChange={(e) => { handleChange(e) }}
+            className='inputText'
+          />
+          <button type="submit" className='submitButton'>作成</button>
+        </form>
+      </div>
+    </div >
   );
 }
 
